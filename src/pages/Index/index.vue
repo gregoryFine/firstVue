f7089cf235406e8874dad9a691d14bbff12422c4
gregoryFine <caoxiaolu2007@163.com>
2019-09-30 17:31:49 +0800

    fix

---
+++ src/pages/Index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <Layout class = "layout full">
+
+    <Layout style = "background:#57b0d8;">
+
+      <Sider ref = "side" style = "overflow-y:scroll !important;" :collapsed-width="80" class = "sider">
+        <Menu :active-name = "menuActiveName" :open-name = "menuOpenNames" ref = "mainMenu" width="auto">
+
+
+
+
+
+        </Menu>
+      </Sider>
+    </Layout>
+
+
+
+  </Layout>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      menu:[
+        {menuId:'1', name:'首页', path:'/home'},
+        {menuId:'2', name:'防火墙管理', items:[{ menuId : '2-1', name : '防火墙信息管理', path:'/firewallBaseMessage'}]},
+      ],
+      menuActiveName: '1',
+      menuOpenNames : []
+    }
+  }
+
+
+
+
+
+};
+</script>
+<style scoped>
+
+.layout{
+  position:relative;
+  overflow:hidden;
+}
+.full {
+  margin:0;
+  padding:0;
+  width:100%;
+  height:100%;
+}
+.sider{
+  border-right:1px solid #eee;
+  background:inherit;
+}
+
+
+</style>
---
+++ src/pages/Login/index.vue
@@ -0,0 +1,66 @@
+<template>
+<div class = "outer_div"> 
+    <div class="inner_div">
+      <Row >
+        <Icon type="md-game-controller-b" size="50" color="red" />
+      </Row>
+      <Row class="mt1">
+        <Input placeholder="请输入姓名" :style="{'width':'auto'}">
+          <Icon type="ios-contact" slot="prefix" />
+        </Input>
+      </Row>
+      <Row class="mt1">
+        <Input placeholder="请输入密码" :style="{'width':'auto'}" type="password">
+          <Icon type="logo-xbox" slot="prefix" />
+        </Input>
+      </Row>
+      <Row class="mt1">
+        <Button type="success" @click="goIndex">登录</Button>
+      </Row>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+      goIndex(){
+          this.$router.push({path:"/index"})
+      }
+  },
+  created() {},
+  mounted() {},
+  components: {}
+};
+</script>
+<style scoped>
+.outer_div{
+    width:100%;
+    height:100%;
+    background:url('../../assets/image/aa.jpg') no-repeat;
+}
+
+.inner_div {
+  width: 30%;
+  height: 30%;
+  position:absolute;
+  left:35%;
+  top:35%;
+  border-radius: 5px;
+  margin:0 auto;
+  border: solid 1px #653a3a;
+  background:url('../../assets/image/bb.jpg') no-repeat;
+  background-size: 100% 100%;
+}
+
+.mt1 {
+  margin-top: 20px;
+}
+.mt5 {
+    margin-top:50px;
+}
+</style>
